073fcf27a69a589013906f25419dce70f7cbb093
senbet22 <senbet22@users.noreply.github.com>
2026-04-10 09:45:40 +0200

    Added model representing our data structure.

--- Models/UfoSighting.cs
+++ Models/UfoSighting.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace UFO_Records_Analyzer.Models
+﻿namespace UFO_Records_Analyzer.Models
 {
     internal class UfoSighting
     {
+        // datetime,city,state,country,shape,duration (seconds),duration (hours/min),comments,date posted,latitude,longitude
+        public DateTime DateTime { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? Shape { get; set; }
+        public double DurationSeconds { get; set; }
+        public string? DurationHoursMin { get; set; }
+        public string? Comments { get; set; }
+        public DateTime DatePosted { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
