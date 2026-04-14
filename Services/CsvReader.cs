3ecd68518553c9a0be48ecfe7c526b465a6be3c4
senbet22 <senbet22@users.noreply.github.com>
2026-04-14 17:43:52 +0200

    Added CsvReader service that parses the raw data using the model.

--- Services/CsvReader.cs
+++ Services/CsvReader.cs
@@ -1,10 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using UFO_Records_Analyzer.Models;
 
 namespace UFO_Records_Analyzer.Services
 {
-    internal class CsvReader
+    public class CsvReader
     {
+        public List<UfoSighting> ReadCsv(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            var data = new List<UfoSighting>();
+
+            foreach (var line in lines.Skip(1))
+            {
+                var parts = line.Split(',');
+
+                // Skip bad rows
+                if (parts.Length < 11)
+                    continue;
+
+                double.TryParse(parts[5], out var duration);
+                double.TryParse(parts[9], out var latitude);
+                double.TryParse(parts[10], out var longitude);
+
+                DateTime.TryParse(parts[0], out var dateTime);
+                DateTime.TryParse(parts[8], out var datePosted);
+
+                var sighting = new UfoSighting
+                {
+                    DateTime = dateTime,
+                    City = parts[1],
+                    State = parts[2],
+                    Country = parts[3],
+                    Shape = parts[4],
+                    DurationSeconds = duration,
+                    Comments = parts[7],
+                    DatePosted = datePosted,
+                    Latitude = latitude,
+                    Longitude = longitude
+                };
+
+                data.Add(sighting);
+            }
+
+            return data;
+        }
     }
 }
+
